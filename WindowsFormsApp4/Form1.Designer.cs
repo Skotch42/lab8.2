d78432a4e0a03516a2c737bb3d9e0f727d0f5dbc
Комардин Влад <99380380+Skotch42@users.noreply.github.com>
2023-05-05 21:39:32 +0700

    Add files via upload

---
+++ WindowsFormsApp4/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace WindowsFormsApp4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Rockwell", 14.25F);
+            this.label1.ForeColor = System.Drawing.Color.Green;
+            this.label1.Location = new System.Drawing.Point(48, 163);
+            this.label1.MinimumSize = new System.Drawing.Size(30, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(334, 49);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Да!";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Visible = false;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(159, 108);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(106, 43);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Спросить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(64, 66);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(299, 22);
+            this.textBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(431, 235);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ WindowsFormsApp4/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random generator = new Random();
+            int number = generator.Next(1, 100);
+            label1.Visible = true;
+
+            int i = 0;
+            int[] prob = new int[8];
+            prob[0] = 15;
+            prob[1] = 12;
+            prob[2] = 8;
+            prob[3] = 17;
+            prob[4] = 4;
+            prob[5] = 11;
+            prob[6] = 16;
+            prob[7] = 17;
+
+            if (textBox1.Text.Length == 0)
+            {
+                label1.ForeColor = Color.Black;
+                label1.Text = "Сначала введите вопрос!";
+            }
+            else
+            {
+                do
+                {
+                    number -= prob[i];
+                    i++;
+                } while (number > 0);
+
+                switch (i)
+                {
+                    case 1:
+                        label1.ForeColor = Color.Blue;
+                        label1.Text = "Никаких сомнений!";
+                        break;
+                    case 2:
+                        label1.ForeColor = Color.Blue;
+                        label1.Text = "Определённо да!";
+                        break;
+                    case 3:
+                        label1.ForeColor = Color.Green;
+                        label1.Text = "Мне кажется — «да»!";
+                        break;
+                    case 4:
+                        label1.ForeColor = Color.Green;
+                        label1.Text = "Вероятнее всего!";
+                        break;
+                    case 5:
+                        label1.ForeColor = Color.Orange;
+                        label1.Text = "Спроси позже!";
+                        break;
+                    case 6:
+                        label1.ForeColor = Color.Orange;
+                        label1.Text = "Пока не ясно, попробуй снова!";
+                        break;
+                    case 7:
+                        label1.ForeColor = Color.IndianRed;
+                        label1.Text = "Мой ответ — «нет»!";
+                        break;
+                    case 8:
+                        label1.ForeColor = Color.IndianRed;
+                        label1.Text = "Даже не думай!";
+                        break;
+                }
+            }
+
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
